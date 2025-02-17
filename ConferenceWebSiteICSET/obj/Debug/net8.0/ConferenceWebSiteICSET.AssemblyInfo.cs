ffa8587b4c7c3b7a41e7a8e16c96e17032352486
vasanth-k20 <vasanthvvkk03@gmail.com>
2025-02-18 05:09:39 +0530

    3rdcmt

--- ConferenceWebSiteICSET/obj/Debug/net8.0/ConferenceWebSiteICSET.AssemblyInfo.cs
+++ ConferenceWebSiteICSET/obj/Debug/net8.0/ConferenceWebSiteICSET.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceWebSiteICSET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13d8fed325a45ef1ca309cf2a004ec73e432c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa93550a90b24989d61960597a148d6b8126c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceWebSiteICSET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceWebSiteICSET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
